d44e2e22687f0d71a8db8ffbe1ed00a259f23ece
dbadoy <72970043+dbadoy@users.noreply.github.com>
2021-01-03 23:15:07 +0900

    Create transaction.js

---
+++ eosjs/transaction.js
@@ -0,0 +1,41 @@
+// npm install eosjs@16.0.9
+
+const Eos = require('eosjs');
+
+// set eos config
+const eos_config = {
+    expireInSeconds: 60,
+    broadcast: true,
+    debug: false,
+    sign: true,
+    httpEndpoint: 'https://jungle3.cryptolions.io:443',
+    keyProvider: [
+        key
+    ], 
+    chainId: '2a02a0053e5a8cf73a56ba0fda11e4d92e0238a4a2aa74fccf46d5a910746840',
+};  // Jungle3's chainId
+
+
+function myTx() {
+  const eos = Eos(eos_config);
+  
+  eos.transaction({
+                actions: [{
+                    account: "[ACCOUNT_NAME]",
+                    name: "[ACTION_NAME]",
+                    data: {
+                        //[ACTION_PARAMETER] : [VALUE],
+                        //.
+                        //.
+                        //.
+                        // example
+                        user: "examaccount1",
+                        from: "examaccount2",
+                    },
+                    authorization: [{
+                        actor: [ACCOUNT_NAME],
+                        permission: "[active / owner ]"
+                    }]
+                }]
+            })
+}
